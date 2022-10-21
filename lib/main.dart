198dd57d86d6aa8ccc45533482d758346122e832
paul.marniquet <paul.marniquet@epitech.eu>
2022-10-21 16:42:52 +0200

    [+] Created multiple windows [+]

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(MaterialApp(
+      home: MyApp(),
+      theme: themeData,
+  ));
+}
+final ThemeData themeData = ThemeData(
+  canvasColor: Colors.lightGreenAccent,
+);
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,19 +17,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
 
-    return MaterialApp(
-      color: Colors.blue,
-      title: 'Featurine',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Featurine'),
-          centerTitle: true,
-          toolbarHeight :100,
-        ),
+    return Scaffold(
         body: Center(
+          child: TextButton(
+            onPressed: (){
+              Navigator.push(context, PageTwo());
+            },
           child: Text(wordPair.asPascalCase),
+          )
+    )
+    );
+  }
+}
+
+class PageTwo extends MaterialPageRoute<void> {
+  PageTwo() : super(builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 1.0,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.push(context, PageThree());
+          },
+          child: Text('Page Three'),
         ),
       ),
     );
-  }
+  });
+}
+
+class PageThree extends MaterialPageRoute<void> {
+  PageThree() : super(builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .accentColor,
+        elevation: 2.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+          },
+          child: Text('Back home'),
+        ),
+      ),
+    );
+  });
 }
