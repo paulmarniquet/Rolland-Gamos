633ef565e068fbe731413469eaa2c4d234ce29b8
paul.marniquet <paul.marniquet@epitech.eu>
2022-10-24 13:09:39 +0200

    [+] Print logo on Homepage [+]

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,82 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-      home: MyApp(),
-      theme: themeData,
+    home: Home(),
+    theme: themeData,
   ));
 }
+
 final ThemeData themeData = ThemeData(
   canvasColor: Colors.lightGreenAccent,
 );
 
-class MyApp extends StatelessWidget {
+Image logo = Image.network(
+    "https://addons-redbullrapjeuopenworld.redbull.com/assets/images/intro/logo-rap-jeu.png", width: 300, height: 300);
+
+class Home extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
-    return Scaffold(
-        body: Center(
-          child: TextButton(
-            onPressed: (){
-              Navigator.push(context, PageTwo());
-            },
-          child: Text(wordPair.asPascalCase),
-          )
-    )
-    );
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+                    child: Stack(children: [
+      GestureDetector(
+        onTap: () {Navigator.push(context, PageTwo());},
+        child: logo,
+      )
+    ]))));
   }
 }
 
 class PageTwo extends MaterialPageRoute<void> {
-  PageTwo() : super(builder: (BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).canvasColor,
-        elevation: 1.0,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            Navigator.push(context, PageThree());
-          },
-          child: Text('Page Three'),
-        ),
-      ),
+  PageTwo()
+      : super(builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+          ),
+                body: Center(
+                    child: Stack(children: [
+        GestureDetector(onTap: () {Navigator.push(context, PageThree());},
+        child: Image.network(
+            "https://addons-redbullrapjeuopenworld.redbull.com/assets/images/intro/logo-rap-jeu.png", width: 300, height: 300),
+      )
+    ])
+    ),
+    backgroundColor: Colors.yellow,
     );
-  });
+        });
 }
 
 class PageThree extends MaterialPageRoute<void> {
-  PageThree() : super(builder: (BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .accentColor,
-        elevation: 2.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
-          },
-          child: Text('Back home'),
-        ),
-      ),
-    );
-  });
+  PageThree()
+      : super(builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).accentColor,
+              elevation: 2.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+            body: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.popUntil(
+                      context, ModalRoute.withName(Navigator.defaultRouteName));
+                },
+                child: Text('Back home'),
+              ),
+            ),
+          );
+        });
 }
